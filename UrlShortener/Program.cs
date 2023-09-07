636d0ac23102dc2d2dacdee87c3377af69d859a0
gamzenurdemir <demirgamzenn@gmail.com>
2023-09-07 14:39:30 +0300

    task-3 add getting url with shortenedUrl

--- UrlShortener/Program.cs
+++ UrlShortener/Program.cs
@@ -53,6 +53,18 @@ app.MapPost("api/shorten", async (
     return Results.Ok(shotenedUrl.ShortUrl);
 });
 
+app.MapGet("api/{code}", async (string code, ApplicationDbContext dbContext) =>
+{
+    var shortenedUrl = await dbContext.ShortenedUrls.FirstOrDefaultAsync(s => s.Code == code);
+
+    if (shortenedUrl is null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Redirect(shortenedUrl.LongUrl);
+});
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
